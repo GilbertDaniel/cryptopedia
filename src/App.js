5eb6df6be17c618c0e75a16e8c0a222c84ed307c
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-06-06 20:23:23 +0630

    Fix Navbar-2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link, Route, Routes } from 'react-router-dom'
-import { Layout, Typography, Space } from 'antd'
+import { Layout, Typography, Space, Button, Menu, Avatar } from 'antd'
 import { HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+import icon from './images/cryptocurrency.png'
 import './App.css'
-import Navbar from './components/Navbar';
 
 const App = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null);
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (screenSize <= 800) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize]);
   return (
     <div className='app'>
       <div className='navbar'>
-        <Navbar />
+        <div className='nav-container'>
+          <div className='logo-container'>
+            <Avatar src={icon} size="large" />
+            <Typography.Title level={2} className='logo'>
+              <Link to="/">Cryptopedia</Link>
+            </Typography.Title>
+            <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
+          </div>
+          {activeMenu && (
+            <Menu theme="dark">
+              <Menu.Item icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+              </Menu.Item>
+              <Menu.Item icon={<FundOutlined />}>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+              </Menu.Item>
+              {/* <Menu.Item icon={<MoneyCollectOutlined />}>
+                        <Link to="/exchanges">Exchanges</Link>
+                    </Menu.Item> */}
+              <Menu.Item icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
+              </Menu.Item>
+            </Menu>
+          )}
+        </div>
       </div>
       <div className='main'>
         <Layout>
@@ -26,7 +72,7 @@ const App = () => {
         <div className='footer'>
           <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2022
             <Link to="/">
-               Cryptoverse Inc.
+              Cryptoverse Inc.
             </Link> <br />
             All Rights Reserved.
           </Typography.Title>
