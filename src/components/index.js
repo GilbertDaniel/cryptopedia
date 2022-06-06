b493b2753e600addc449e76f492c4e5f3b38027d
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-06-06 20:25:11 +0630

    Fix navbar-3

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as HomePage } from './HomePage';
-export { default as Navbar } from './Navbar';
 export { default as Cryptocurrencies } from './Cryptocurrencies';
 export { default as Exchanges } from './Exchanges';
 export { default as CryptoDetails } from './CryptoDetails';
