88c8e3f7350b2808692a246194359078c42f7ca6
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-06-06 17:54:55 +0630

    Implementing Footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
-import {Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News} from './components'
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
 import './App.css'
 
 const App = () => {
   return (
     <div className='app'>
-        <div className='navbar'>
-            <Navbar/>
-        </div>
-        <div className='main'>
-            <Layout>
-              <div className='routes'>
-                  <Routes>
-                    <Route exact path="/" element={<HomePage />}></Route>
-                    <Route exact path="/exchanges" element={<Exchanges />}></Route>
-                    <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />}></Route>
-                    <Route exact path="/crypto/:coinId" element={<CryptoDetails />}></Route>
-                    <Route exact path="/news" element={<News />}></Route>
-                  </Routes>
-              </div>
-            </Layout>
-        </div>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path="/" element={<HomePage />}></Route>
+              <Route exact path="/exchanges" element={<Exchanges />}></Route>
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />}></Route>
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />}></Route>
+              <Route exact path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
         <div className='footer'>
-
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+            <Link to="/">
+              Cryptoverse Inc.
+            </Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
         </div>
+      </div>
+
     </div>
   )
 }
